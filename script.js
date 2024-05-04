5f3135d880eb59b5fe117ea8b378eac69a88b757
hosseinkhashaypour <154625781+hosseinkhashaypour@users.noreply.github.com>
2024-05-04 14:07:12 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,74 @@
-const Myproducts = document.querySelector(".products");
-const buyproducts = document.querySelector(".product-click");
-const inputcategory = document.querySelector('input')
-let imageCount = 0;
+const productDetails = document.querySelector(".product-deatails");
+const categoryOption = document.getElementById("categoryha"); 
+let showProducts = [];
 
 const Myapi = async () => {
     try {
-        const res = await fetch('https://fakestoreapi.com/products');
-        const json = await res.json();
-        console.log(json);
-        return json;
+        const response = await fetch('https://fakestoreapi.com/products');
+        const json = await response.json();
+        showProducts = json;
+        console.log(showProducts);
+        return json
+
     } catch (error) {
-        console.log("error");
+        console.log("Error encountered:", error);
     }
-};
-
-const showData = async () => {
-    const products = await Myapi();
-    products.forEach(product => {
-        if (imageCount < 4) {
-            const productName = product.title;
-            const ProductImg = product.image;
-            const productprice = product.price;
-            const productContent = `<div onclick="selectedproduct('${ProductImg}')" style="border: 1px solid orangered; background-color: rgb(255, 94, 0); border-radius: 10px; padding: 10px 20px;">
-                <img src="${ProductImg}" class="img-style">
-                <br>
-                <h1 class="h-style">${productName}</h1>
-                <p1 class="price" onclick="selectedproduct('${productprice}')">${productprice + "$"}</p1>
-            </div>`;
-            Myproducts.insertAdjacentHTML('beforeend', productContent);
-        }
-    });
-
-    const selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
-    if (selectedProduct) {
-        const selectContent = `<div class="border-div" style="border: 2px solid orange;">
-            <img src="${selectedProduct.image}" class="img-style">
-        </div>`;
-        document.body.insertAdjacentHTML('beforeend', selectContent);
-    }
-};
-function selectedproduct(ProductImg) {
-    localStorage.setItem('selectedProduct', JSON.stringify({ image : ProductImg }));
-    localStorage.getItem('selectedProduct')
-    const selectContent = `<div class="border-div" style="border: 2px solid orange;">
-        <img src="${ProductImg}" class="img-style">
-    </div>`;
-    document.body.insertAdjacentHTML('beforeend', selectContent);
-
 }
+
+const showData = (json) => {
+    try {
+        const selectedCategory = categoryOption.value;
+        const filteredProducts = showProducts.filter(product => product.category === selectedCategory);
+        productDetails.innerHTML = '';
+
+        filteredProducts.forEach(product => {
+            const productTitle = product.title;
+            const productImg = product.image;
+            const productPrice = product.price;
+            const productContent = `
+                <div style="background-color: whitesmoke; height: 150%">
+                    <img src="${productImg}" class="img-style">
+                    <h1 class="h-style">${productTitle}</h1>
+                    <p1 class="price">${productPrice + "$"}</p1>
+                </div>`;
+            productDetails.insertAdjacentHTML("beforeend", productContent);
+
+            const productDiv = productDetails.lastElementChild;
+            productDiv.addEventListener('click', () => {
+                Basket(productTitle, productImg, productPrice);
+            });
+        });
+    } catch (error) {
+        console.error("Error occurred:", error);
+    }
+}
+
+function Basket(productTitle, productImg, productPrice) {
+    const buyedProduct = `
+        <div class="buyed" style = "height : 150%; background-color : whitesmoke;">
+            <img src="${productImg}" class="img-style">
+            <p2 style = "color : white; background-color : red;border-radius : 296px; padding : 10px 20px;font-weight : bold; ">Saved in your basket</p2>
+            <h1 class = "h-style">${productTitle}</h1>
+            <p1 class="price">${productPrice + "$"}</p1>
+            <hr><br>
+        </div>
+    `;
+    const hrtag = `<hr><br>
+    <p1>Your Basket</p1>
+    `
+    productDetails.insertAdjacentHTML("beforeend", buyedProduct);
+    //get Info to browser
+    localStorage.setItem("productInfo" , productTitle ) ;
+    localStorage.setItem("productImg" , productImg);
+    localStorage.setItem("productPrice" , productPrice)
+
+    //recive data from browser
+
+    let reciveTitle = localStorage.getItem("productInfo")
+    let reciveImg = localStorage.getItem("productImg")
+    let recivePrice = localStorage.getItem("productPrice")
+}
+
+Myapi(); 
 showData();
+
