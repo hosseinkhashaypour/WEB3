53534b1efe2455e08d0c2744f7eb5d73790c79a5
hosseinkhashaypour <154625781+hosseinkhashaypour@users.noreply.github.com>
2024-05-11 19:44:15 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 const productDetails = document.querySelector(".product-deatails");
 const categoryOption = document.getElementById("categoryha");
 const spanPrice = document.querySelector(".spanPrice")
+const basketImg = document.querySelector(".basket")
 let showProducts = [];
 
 const Myapi = async () => {
@@ -26,10 +28,12 @@ const showData = () => {
             const productTitle = product.title;
             const productImg = product.image;
             const productPrice = product.price;
+            const productId = product.id
             const productContent = `
                 <div style="background-color: whitesmoke; height: 150%">
                     <img src="${productImg}" class="img-style">
                     <h1 class="h-style">${productTitle}</h1>
+                    <button id = "addBasket">Add to cart</button>
                     <p1 class="price">${productPrice + "$"}</p1>
                 </div>`;
             productDetails.insertAdjacentHTML("beforeend", productContent);
@@ -43,7 +47,32 @@ const showData = () => {
         console.error("Error occurred:", error);
     }
 };
+// const showAllProducts = async ()=>{
+//     try {
+//         const ShowAllApi = await fetch('https://fakestoreapi.com/products')
+//         const ShowAllApiRes = await ShowAllApi.json()
 
+//         ShowAllApiRes.forEach(product =>{
+//             const productTitle = product.title;
+//             const productImg = product.image;
+//             const productPrice = product.price;
+//             const productId = product.id
+//             const productContent = `
+//             <div style="background-color: whitesmoke; height: 150%">
+//                 <img src="${productImg}" class="img-style">
+//                 <h1 class="h-style">${productTitle}</h1>
+//                 <button id = "addBasket">Add to cart</button>
+//                 <p1 class="price">${productPrice + "$"}</p1>
+//             </div>`;
+//             productDetails.insertAdjacentHTML("beforeend", productContent);
+//         })
+//     } catch (error) {
+//         console.log("ridi");
+//     }
+// }
+// document.addEventListener("DOMContentLoaded" , ()=>{
+//     showAllProducts()
+// })
 let basketItems = [];
 let totalPrice = 0;
 let totalSpan = document.createElement('span');
@@ -52,12 +81,15 @@ spanPrice.appendChild(totalSpan);
 
 function Basket(productTitle, productImg, productPrice) {
     const buyedProduct = `
-        <div class="buyed" style="height: 150%; background-color: whitesmoke;">
+        <div class="buyed" style="height: fit-content; background-color: whitesmoke;">
             <img src="${productImg}" class="img-style">
             <p2 style="color: white; background-color: #04aa6d;border-radius: 296px; padding: 10px 20px;font-weight: bold;">Saved in your basket</p2>
             <h1 class="h-style">${productTitle}</h1>
             <p1 class="price">${productPrice + "$"}</p1>
-            <button id = "remove" onclick = "removeCart()">Cancel</button>
+            <br>
+            <div class = "prodctCounter">
+            </div>
+            <button id = "remove">Cancel</button>
             <hr><br>
         </div>
     `;
@@ -67,10 +99,13 @@ function Basket(productTitle, productImg, productPrice) {
 
     totalPrice += productPrice;
     totalSpan.textContent = `Total price is: ${totalPrice.toFixed(2)}$`;
-    // removeCart(productImg, productTitle,productPrice)
 }
 
-
+function removeAll() {
+    localStorage.clear()
+    productDetails.innerHTML = ""
+    location.reload()
+}
 
 let itemGetCount = {};
 function loadBasket() {
@@ -86,11 +121,37 @@ function loadBasket() {
             itemGetCount[basketItem.title] = (itemGetCount[basketItem.title] || 0) + 2;
         });
     }
+    console.log(getBasket);
 }
 
 
+
+basketImg.addEventListener('click', () => {
+    location.reload()
+})
 categoryOption.addEventListener('change', showData);
 document.addEventListener('DOMContentLoaded', loadBasket);
+
+const delProduct = async (productId) => {
+    await fetch(`https://fakestoreapi.com/carts/${productId}`, {
+        method: "DELETE"
+    })
+        .then(res => res.json())
+        .then(json => console.log(json))
+}
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const deleteButton = document.querySelector('#remove');
+//     console.log(deleteButton);
+//     const productIdToDelete = 1; 
+//     delProduct(productIdToDelete)
+//     deleteButton.addEventListener('click' , ()=>{
+//         const producttoDel = deleteButton.parentNode;
+//         localStorage.removeItem("product_" + productIdToDelete);
+//         producttoDel.remove();
+//     });
+// });
 
 Myapi()
 showData()
